613b9e2ae5bbdf6627fb55a1aa8e4d3e2fef56fd
Julian Rivera <julianarevircoins@gmail.com>
2023-07-23 13:52:00 -0400

    styling/route/page for userDetails

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -6,8 +6,33 @@ import { useParams } from "react-router-dom";
 export const UserDetails = () => {
   const [userName, setUserName] = useState("");
   const [tasks, setTasks] = useState(null);
+  const [priority, setPriority] = useState("");
+
+  const selectPriority = (e) => {
+    setPriority(e.target.value);
+  };
   const { id } = useParams();
   console.log(id);
+
+  const updatePriority = (priority, id) => {
+    axios
+      .post("http://localhost:4000/users/user/update", { priority, id })
+      .then((response) => {
+        console.log(response);
+        const newTasks = tasks.map((task) => {
+          if (task.id === id) {
+            return { ...task, priority: priority };
+          }
+          return task;
+        });
+        setTasks(newTasks);
+        console.log(newTasks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const getDetails = () => {
     axios
       .get(`http://localhost:4000/users/user/${id}`)
@@ -29,23 +54,76 @@ export const UserDetails = () => {
     getDetails();
   }, []);
   console.log(userName);
-
+  console.log(priority);
   return (
     <>
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ display: "flex", alignItems: "center", color: "white" }}>
         user:
         <h2>{userName}</h2>
       </div>
-      <div style={{ width: "100vw" }}>
+      <div style={{ width: "90vw", display: "flex", justifyContent: "center" }}>
         tasks:
-        {tasks &&
-          tasks.map((task) => {
-            return (
-              <>
-                <p>{task.task_name}</p>
-              </>
-            );
-          })}
+        <table style={{ color: "white" }}>
+          <tr
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "50vw",
+            }}
+          >
+            <td>Task ID</td>
+            <td>Task Name</td>
+            <td>Priority</td>
+          </tr>
+          {tasks &&
+            tasks.map((task) => {
+              return (
+                <>
+                  <tr
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      width: "50vw",
+                    }}
+                  >
+                    <td>{task.id}</td>
+                    <td>{task.task_name}</td>
+                    <select
+                      onChange={(e) => {
+                        updatePriority(e.target.value, task.id);
+                      }}
+                      style={{ backgroundColor: "#176db7", height: "100%" }}
+                      value={task.priority}
+                    >
+                      <option value="High">High</option>
+                      <option value="Medium">Medium</option>
+                      <option value="Low">Low</option>
+                    </select>
+                  </tr>
+
+                  {/* <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-evenly",
+                      width: "100vw",
+                    }}
+                  >
+                    <p style={{ color: "white" }}>{task.task_name}</p>
+
+                    <select
+                      // onChange={selectPriority}
+                      style={{ backgroundColor: "#176db7", height: "100%" }}
+                    >
+                      <option value="High">High</option>
+                      <option value="Medium">Medium</option>
+                      <option value="Low">Low</option>
+                    </select>
+                  </div> */}
+                </>
+              );
+            })}
+        </table>
       </div>
     </>
   );
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -7,7 +7,11 @@ export const Users = () => {
   const [users, setUsers] = useState(null);
   const [toggle, setToggle] = useState(false);
   const [taskName, setTaskName] = useState("");
+  const [priority, setPriority] = useState("");
 
+  const selectPriority = (e) => {
+    setPriority(e.target.value);
+  };
   const userTaskBool = (id) => {
     console.log(id);
     axios
@@ -61,13 +65,22 @@ export const Users = () => {
       });
   }, []);
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+      }}
+    >
       Users
       <AddUser setUsers={setUsers} users={users} />
       {users?.map((user) => {
         return (
           <>
-            <div style={{ display: "flex" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
               <Link to={`/users/${user.id}`}>
                 <p>
                   {user.first_name}
@@ -88,14 +101,46 @@ export const Users = () => {
                     e.preventDefault();
                     assignTask(taskName, user.id);
                   }}
+                  style={{
+                    display: "flex",
+                    height: "50px",
+                    alignItems: "center",
+                  }}
                 >
+                  {/* <div
+                    style={{
+                      display: "flex",
+                      height: "100%",
+                      backgroundColor: "#176db7",
+                    }}
+                  > */}
+                  <h4
+                    style={{
+                      backgroundColor: "#176db7",
+                      height: "100%",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    Priority
+                  </h4>
+                  <select
+                    onChange={selectPriority}
+                    style={{ backgroundColor: "#176db7", height: "100%" }}
+                  >
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                  </select>
+                  {/* </div> */}
                   <input
                     type="text"
                     placeholder="Task to assign..."
                     name="taskName"
                     onChange={onChangeTask}
+                    style={{ height: "90%" }}
                   ></input>
-                  <button>Click to Assign</button>
+                  <button style={{ height: "100%" }}>Click to Assign</button>
                 </form>
               ) : (
                 ""
